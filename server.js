f20dacf7b478430d29cb59c9f1c5ba738b1c57e8
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-25 17:08:26 -0600

    still having odd issue connecting to routes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const apiRoutes = require("./routes/api.js");
+//const apiRoutes = require("./routes/api.js");
 
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa";
+//const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa";
 
 const app = express();
 
@@ -18,16 +18,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// });
+
 // routes
-//app.use(require("./routes/api.js"));
-app.use(apiRoutes);
+app.use(require("./routes/api.js"));
+//app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
