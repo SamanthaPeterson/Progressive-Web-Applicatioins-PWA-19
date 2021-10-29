7f3719f08f8168768cb9604247c99a3bb610b0b3
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-28 19:28:34 -0600

    update server js

--- server.js
+++ server.js
@@ -1,28 +1,55 @@
 const express = require("express");
-const logger = require("morgan");
+//const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
-
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa";
+//const compression = require("compression");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(logger("dev"));
+const {
+  Note
+} = require('./models');
 
-app.use(compression());
+// app.use(logger("dev"));
+
+// app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-pwa", {
+useFindAndModify: false,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.post('/api/submit', ({
+  body
+}, res) => {
+  Note.create(body)
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get('/api/all', (req, res) => {
+  Note.find({})
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
